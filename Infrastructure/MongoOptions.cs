d3767fe49a20b35467e963093a81a232666c31ed
4E6ynEJlb <seconda034@gmail.com>
2024-10-26 02:34:44 +0300

    Added Infrastructure  project

---
+++ Infrastructure/MongoOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure
+{
+    public class MongoOptions
+    {
+        public const string OptionsName = "MongoOptions";
+        public required string Endpoint { get; set; }
+        public required string Database { get; set; }
+        public required string OrdersCollection { get; set; }
+    }
+}
---
+++ Infrastructure/ShippingContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models.Entities;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Infrastructure
+{
+    public class ShippingContext
+    {
+        public readonly IQueryable<Order> Orders;
+        public ShippingContext(IOptions<MongoOptions> options) 
+        {
+            MongoClient client = new MongoClient(options.Value.Endpoint);
+            IMongoDatabase database = client.GetDatabase(options.Value.Database);
+            database.CreateCollection(options.Value.OrdersCollection);
+            Orders = database.GetCollection<Order>(options.Value.OrdersCollection)
+                .AsQueryable();
+        }
+    }
+}
